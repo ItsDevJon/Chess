0990298dff49fbd7ec3895023b8fa2af236e4e5f
Eneko <eneko.zugazaga@alumni.mondragon.edu>
2024-10-31 09:33:46 +0100

    Create Mouse Handler and Add Resources

---
+++ src/main/java/com/chess/MouseHandler.java
@@ -0,0 +1,55 @@
+package com.chess;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MouseHandler implements MouseListener {
+
+    private boolean fromToTurnstale = true;
+    int xFrom;
+    int yFrom;
+    int xTo;
+    int yTo;
+    boolean clicked = false;
+    boolean selected = false;
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (fromToTurnstale) {
+            xFrom = e.getX();
+            yFrom = e.getY();
+            selected = true;
+        } else {
+            selected = false;
+            xTo = e.getX();
+            yTo = e.getY();
+            clicked = true;
+        }
+        fromToTurnstale = !fromToTurnstale;
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mousePressed'");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseReleased'");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseEntered'");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseExited'");
+    }
+    
+}
