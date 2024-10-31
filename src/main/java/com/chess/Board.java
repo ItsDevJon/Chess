ff4954c74204c833123035a43faf831f45331d3b
unai <unaidieguez03@gmail.com>
2024-10-31 09:30:07 +0100

    board implementation

---
+++ src/main/java/com/chess/Board.java
@@ -0,0 +1,32 @@
+package com.chess;
+import java.awt.Graphics2D;
+import java.awt.Color;
+public class Board {
+    public final static int totalRow = 8;
+    public final static int totalCol = 8;
+    public final static int totalPiece = totalCol * totalRow;
+    GamePanel gp;
+    //TODO: GET list<Piece> chessBoard and initialize
+    public Board(GamePanel gp) {
+        this.gp = gp;
+        System.out.println("asi da");
+        chessInit();
+    }
+
+    public void chessInit() {
+        //TODO: INIT list INIT
+        System.out.println("init list");
+    }
+    public void drawBoard(Graphics2D g2){
+        boolean turnTales = true;
+        for (int y = 0; y < Chess.totalCol; y++) {
+            for (int x = 0; x < Chess.totalRow; x++) {
+                g2.setColor(turnTales ? Color.BLACK : Color.WHITE);
+                g2.fillRect(x * gp.tileSize, y * gp.tileSize, gp.tileSize,
+                gp.tileSize);
+                turnTales = !turnTales;
+            }
+            turnTales = !turnTales;
+        }
+    }
+}
--- src/main/java/com/chess/Chess.java
+++ src/main/java/com/chess/Chess.java
@@ -1,32 +1,22 @@
 package com.chess;
 
 import java.awt.Graphics2D;
-import java.awt.Color;
 
 public class Chess {
     public static final int totalCol = 8;
     public static final int totalRow = 8;
 
     GamePanel gp;
-
+    Board board;
     public Chess(GamePanel gp){
         this.gp = gp;
+        this.board = new Board(gp);
 
     }
     public void game(){
         
     }
     public void draw(Graphics2D g2) {
-        boolean turnTales = true;
-        for (int y = 0; y < Chess.totalCol; y++) {
-            for (int x = 0; x < Chess.totalRow; x++) {
-                g2.setColor(turnTales ? Color.BLACK : Color.WHITE);
-                g2.fillRect(x * gp.tileSize, y * gp.tileSize, gp.tileSize,
-                gp.tileSize);
-                turnTales = !turnTales;
-            }
-            turnTales = !turnTales;
-        }
-        
+        board.drawBoard(g2);
     }
 }
