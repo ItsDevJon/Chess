27f61f34450228e057c7e39a642b9b8373f5a561
Jon Ramos <134915416+ItsDevJon@users.noreply.github.com>
2024-11-04 08:40:00 +0100

    Add test for Piece.java

--- src/test/java/com/chess/PieceTest.java
+++ src/test/java/com/chess/PieceTest.java
@@ -1,4 +1,25 @@
+package com.chess;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 class PieceTest {
-  
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void testToString() {
+    }
 }
