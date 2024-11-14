0c1ec5848155835e040f87a934df28cdc036fcf1
Iker Marcos <ikermarcos15@gmail.com>
2024-11-14 12:09:52 +0100

    Revert "Clean Piece.java"
    This reverts commit 1b457da6ca4cda77f72ed1b17c3d2ea6fb35a6df.

--- src/main/java/com/chess/pieces/Piece.java
+++ src/main/java/com/chess/pieces/Piece.java
@@ -65,6 +65,17 @@ public abstract class Piece {
         }
     }
     
+    public boolean canMove(Board board, int pieceIndex, int possibleMove) {
+        
+        Optional<Piece> tileToMove = Optional.ofNullable(board.boardPieces.get(possibleMove));
+        if ((tileToMove.isPresent() && tileToMove.get().color != this.color) || tileToMove.isEmpty()) {
+            movePiece(board, pieceIndex, possibleMove);
+            return true;
+        }   else {
+            return false;
+        }
+    }
+    
     public boolean moveDiagonal(Board board, int xTo, int yTo) {
         int xOrigin = (this.x / GamePanel.tileSize);
         int yOrigin = (this.y / GamePanel.tileSize);
@@ -209,5 +220,10 @@ public abstract class Piece {
         img = ImageIO.read(new FileInputStream(BASE.resolve(type + "_" + color + ".png").toString().toLowerCase()));
 
         return img;
+    }
+
+    @Override
+    public String toString() {
+        return "Piece [img=" + img + ", y=" + y + ", x=" + x + ", type=" + type + ", color=" + color + "]";
     }
 }
